85e271efe1eecbd6fd6ca9824e7ead71a893fd68
Lev <fedorichev16@gmail.com>
2022-08-06 19:39:54 +0300

    mey

---
+++ parser/src/main/java/Main.java
@@ -0,0 +1,58 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.*;
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+
+        String json = listToJson(list);
+
+        try (FileWriter file = new FileWriter("data.json")) {
+            file.write(json);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String listToJson(List<Employee> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        Type listType = new TypeToken<ArrayList<Employee>>() {
+        }.getType();
+        return gson.toJson(list, listType);
+    }
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        List<Employee> data = new ArrayList<>();
+
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader).withMappingStrategy(strategy).build();
+            data = csv.parse();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return data;
+    }
+}
